a5183039c2022154a8868a7eb8fa256905008e48
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-04-28 17:47:30 +0700

    User GUI

---
+++ User_GUI.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+Screen = Tk()
+Screen.geometry("400x400")
+Screen.title("Dashboard")
+Label(Screen, text = "Welcome to User Data Management", width = 100, height = 2, bg = "grey", font = ("Calibri", 18)).pack()
+Screen.mainloop()
