78954582606e71f6f9d31a8afa71e92377dba808
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-04-29 01:50:26 +0700

    Update Login and Register

--- User_GUI.py
+++ User_GUI.py
@@ -1,7 +1,26 @@
 from tkinter import *
+def Register():
+    Screen2 = Toplevel(Screen)
+    Screen2.geometry("400x400")
+    Screen2.title("User Register")
+    
+    
+def Login():
+    Screen1 = Toplevel(Screen)
+    Screen1.geometry("400x400")
+    Screen1.title("User Login")
+    
 
-Screen = Tk()
-Screen.geometry("400x400")
-Screen.title("Dashboard")
-Label(Screen, text = "Welcome to User Data Management", width = 100, height = 2, bg = "grey", font = ("Calibri", 18)).pack()
-Screen.mainloop()
+def main():
+    global Screen
+    Screen = Tk()
+    Screen.geometry("400x400")
+    Screen.title("Dashboard")
+    Label(Screen, text = "Welcome to User Data Management", width = 100, height = 2, bg = "grey", font = ("Calibri", 18)).pack()
+    Label(Screen, text = "", width = 100, height = 1).pack()
+    Button(Screen, text = "Login", width = 10, height = 2, command = Login).pack()
+    Label(Screen, text = "", width = 100, height = 1).pack()
+    Button(Screen, text = "Register", width = 10, height = 2, command = Register).pack()
+    Screen.mainloop()
+
+main()
