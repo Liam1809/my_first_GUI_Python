4d30c26caef33b0f17d718e8aad4acf3eb9e20a8
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-04-30 22:38:10 +0700

    Update Login

--- User_GUI.py
+++ User_GUI.py
@@ -3,12 +3,27 @@ def Register():
     Screen2 = Toplevel(Screen)
     Screen2.geometry("400x400")
     Screen2.title("User Register")
+    Label(Screen2, text = "", width = 100, height = 1).pack()
+    Label(Screen2, text = "Login ", width = 100, height = 2, font = ("Calibri",15)).pack()
+    Label(Screen2, text = "", width = 100, height = 1).pack()
+    Entry(Screen2, text = "Password", width = 100, height = 2, font = ("Calibri",15)).pack()
     
     
+def Login_Section():
+    print("") 
 def Login():
     Screen1 = Toplevel(Screen)
     Screen1.geometry("400x400")
     Screen1.title("User Login")
+    Label(Screen1, text = "", width = 100, height = 1).pack()
+    Label(Screen1, text = "Login", width = 100, height = 1, font = ("Calibri",15)).pack()
+    Label(Screen1, text = "", width = 100, height = 1).pack()
+    User_Login = ""
+    Entry(Screen1, text = User_Login, width = 20).pack()
+    Password_Login = ""
+    Entry(Screen1, text = Password_Login, width = 20).pack()
+    Button(Screen1, text = "Login", width = 10, command = Login_Section).pack()
+
     
 
 def main():
